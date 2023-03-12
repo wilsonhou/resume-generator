4ab43ce3f8e45f807408464b575ee3618db1b5eb
Wilson Hou <wilshou@gmail.com>
2023-03-12 22:58:13 +1100

    remove error

--- old.ts
+++
@@ -1,9 +0,0 @@
-// export { default } from "next-auth/middleware";
-// export const middleware = () => null;
-
-// @TODO - get auth working! (middleware gate the entire app, and redirect to custom sign in page!)
-// @TODO - write a regex that excludes the login page.
-// @TODO - redirect all other pages to login page (set the path to /login)
-// export const config = { matcher: ["/"] };
-
-// See "Matching Paths" below to learn more
